77cc0b84f9410a6a27a63d097e54d58c8e2116dc
dakavin <mikeylay@126.com>
2024-08-18 23:31:47 +0800

    feat(Chapter4): 第4章练习题

---
+++ Chapter4/P1-2.c
@@ -0,0 +1,21 @@
+/**
+ * @file P1-2.c
+ * @author dakkk
+ * @brief 
+ * @version 0.1
+ * @date 2024/8/14
+ */
+#define Q "His Hamlet was funny withoutbeing vulgar."
+#define BOOK "War and Peace"
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    size_t a = 7;
+    printf("The double type is %zd bytes..\n", sizeof(double));
+    printf("%z\n", a);
+    return 0;
+}
+
+
+
