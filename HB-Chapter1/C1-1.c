b6951650ee7a18a2179ec01ee5b1dff9717ed377
dakavin <mikeylay@126.com>
2024-08-20 23:54:54 +0800

    feat(Chapter4): 删除冲突的c文件

---
+++ HB-Chapter1/C1-1.c
@@ -0,0 +1,7 @@
+/**
+ * @file C1-1.c
+ * @author dakkk
+ * @brief 
+ * @version 0.1
+ * @date 2024-08-19
+ */
---
+++ HB-Chapter1/C1-2.c
@@ -0,0 +1,12 @@
+/**
+ * @file C1-2.c
+ * @author dakkk
+ * @brief 
+ * @version 0.1
+ * @date 2024-08-19
+ */
+#include <stdio.h>
+int main(void){
+    printf("hello world!\n");
+    return 0;
+}
