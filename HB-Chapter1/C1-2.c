447bc6357dacdec01918227283f60a9c079ad802
dakavin <mikeylay@126.com>
2024-10-21 20:34:55 +0800

    feat(All): 循环的学习

--- HB-Chapter1/C1-2.c
+++ HB-Chapter1/C1-2.c
@@ -8,8 +8,17 @@
 #include <stdio.h>
 #include <stdint.h>
 
+enum Sex{
+    MALE,
+    FEMALE,
+    SECRET
+};
+
 int main(void){
     printf("Hello world!\n");
     printf("%d\n", sizeof(int8_t));
+    printf("%d\n", MALE);
+    printf("%d\n", FEMALE);
+    printf("%d\n", SECRET);
     return 0;
 }
